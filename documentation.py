d9f0318515d5fbef47724b407417f75a6f82ff72
Maxim Emelyanov <emelyanov.mv@dclouds.ru>
2023-10-18 01:01:18 +0700

    docs

--- documentation.py
+++ documentation.py
@@ -1,4 +1,16 @@
-import flet as ft
-
-def create_documentation_tab():
-    return ft.Tab(text="Documentation", content=ft.Container(content=ft.Text("Documentation content")))
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
+from PyQt5.QtCore import QUrl
+import os
+class DocumentationTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.webView = QWebEngineView()
+        self.webView.settings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
+        self.webView.settings().setAttribute(QWebEngineSettings.PdfViewerEnabled, True)
+        #pdf_path = os.path.abspath("/Russian_Railways/docs.pdf")
+        pdf_path = os.path.abspath("../Russian_Railways/docs.pdf")
+        self.webView.setUrl(QUrl.fromLocalFile(pdf_path))
+        layout = QVBoxLayout()
+        layout.addWidget(self.webView)
+        self.setLayout(layout)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget
 from editor import EditorTab
 from history import HistoryTab
 from games_tab import GamesTab
+from documentation import DocumentationTab
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -13,10 +14,12 @@ class MainWindow(QMainWindow):
         editor_tab = EditorTab()
         history_tab = HistoryTab()
         rhythm_game = GamesTab(self)
+        docs_tab = DocumentationTab()
 
         self.tab_widget.addTab(editor_tab, "Editor")
         self.tab_widget.addTab(history_tab, "History")
         self.tab_widget.addTab(rhythm_game, "Игры")
+        self.tab_widget.addTab(docs_tab, "Docs")
 
         self.setCentralWidget(self.tab_widget)
 
