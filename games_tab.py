c50423a8784bf527318220cdc8f2822ab09902d5
Maxim Emelyanov <emelyanov.mv@dclouds.ru>
2023-11-25 22:15:27 +0700

    games preview

--- games_tab.py
+++ games_tab.py
@@ -1,6 +1,7 @@
 import os
 
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QApplication
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QApplication, QLabel
+from PyQt5.QtGui import QPixmap
 # from games.CardGame.card_game import main
 import subprocess
 import sys
@@ -16,10 +17,33 @@ class GamesTab(QWidget):
         self.pushButton.clicked.connect(self.cardgame)
         self.pushButton2.clicked.connect(self.slidinggame)
         self.pushButton3.clicked.connect(self.pianogame)
-        self.layout.addWidget(self.pushButton)
-        self.layout.addWidget(self.pushButton2)
-        self.layout.addWidget(self.pushButton3)
+        buttonLayout = QHBoxLayout()
+        buttonLayout.addWidget(self.pushButton)
+        buttonLayout.addWidget(self.pushButton2)
+        buttonLayout.addWidget(self.pushButton3)
+        self.layout.addLayout(buttonLayout)
         self.setLayout(self.layout)
+        self.setStyleSheet("background-color: red;")
+
+        # Add pictures below the buttons
+        self.pictureLayout = QHBoxLayout()
+        for i in range(1, 4):
+            pixmap = QPixmap(f'GamePics/game{i}.png')
+            label = QLabel()
+            label.setPixmap(pixmap)
+            label.mousePressEvent = lambda event, game=i: self.on_picture_click(game)
+            self.pictureLayout.addWidget(label)
+        
+        # Add pictures to the main layout
+        self.layout.addLayout(self.pictureLayout)
+
+    def on_picture_click(self, game):
+        if game == 1:
+            self.cardgame()
+        elif game == 2:
+            self.slidinggame()
+        elif game == 3:
+            self.pianogame()
 
     def cardgame(self):
         ws = QApplication.allWindows()
@@ -42,8 +66,14 @@ class GamesTab(QWidget):
         for w in ws:
             w.close()
         subprocess.run(f"{sys.executable} games/PianoGame/main.py")
-        # subprocess.run(['python', 'games/PianoGame/main.py'])
         for w in ws:
             w.show()
 
+    def game1(self):
+        self.cardgame()
 
+    def game2(self):
+        self.slidinggame()
+
+    def game3(self):
+        self.pianogame()
