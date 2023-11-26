ca6cf3cc7904aa854dfcf84a742fbe521d08bc20
Maxim Emelyanov <emelyanov.mv@dclouds.ru>
2023-11-26 23:47:26 +0700

    story tab

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget
 from editor import EditorTab
 from history import HistoryTab
+from story import StoryTab
 from games_tab import GamesTab
 from documentation import DocumentationTab
 from leaderboard import LeaderboardTab 
@@ -14,15 +15,17 @@ class MainWindow(QMainWindow):
 
         editor_tab = EditorTab()
         history_tab = HistoryTab()
+        story_tab = StoryTab()
         rhythm_game = GamesTab(self)
         docs_tab = DocumentationTab()
         leaderboard_tab = LeaderboardTab() 
 
         self.tab_widget.addTab(editor_tab, "Editor")
-        self.tab_widget.addTab(history_tab, "History")
+        self.tab_widget.addTab(history_tab, "История Карт")
+        self.tab_widget.addTab(story_tab, "Сюжет")
         self.tab_widget.addTab(rhythm_game, "Игры")
-        self.tab_widget.addTab(docs_tab, "Docs")
-        self.tab_widget.addTab(leaderboard_tab, "Leaderboard")
+        self.tab_widget.addTab(docs_tab, "Документация")
+        self.tab_widget.addTab(leaderboard_tab, "Статистика")
 
         self.setCentralWidget(self.tab_widget)
         
---
+++ story.py
@@ -0,0 +1,62 @@
+
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel, QHBoxLayout
+from PyQt5.QtGui import QPixmap, QFont
+
+class StoryTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.layout = QVBoxLayout()
+
+        # Add a QLabel for the story text about the train at the top of the window
+        storyLabel = QLabel("<font size=40><b>Тууу туу, на ваши плечи легло ответственное задание - переправить уголь из Кузбасса в Камбоджу!</b></font>")
+        storyLabels = QLabel("<font size=40><b>Чтож, говорят вы спец.., надеемся для вас не составит труда пройти интересные задания на вашем пути! Удачи!</b></font>")
+        storyLabel.setFont(QFont("Arial", 6, QFont.Bold))
+        storyLabels.setFont(QFont("Arial", 6, QFont.Bold))  # Set the font to bold
+        self.layout.addWidget(storyLabel)
+        self.layout.addWidget(storyLabels)
+
+        # Create a QHBoxLayout for the picture and the buttons
+        pictureAndButtonsLayout = QHBoxLayout()
+
+        # Add a QLabel for the picture
+        pixmap = QPixmap('GamePics/map.png')
+        pixmap_bigger = pixmap.scaled(pixmap.width() * 1.2, pixmap.height() * 1.2)  # Make the picture 1.5 times bigger
+        label = QLabel()
+        label.setPixmap(pixmap_bigger)
+        pictureAndButtonsLayout.addWidget(label)
+
+        # Create a QVBoxLayout for the buttons
+        buttonLayout = QVBoxLayout()
+
+        # Create 5 buttons with bold font, round corners, and the specified names
+        buttons = []
+        button_names = ["КУЗБАСС", "КАМЕРУН", "КУБА", "КОРСИКА", "КАМБОДЖА"]
+        for name in button_names:
+            button = QPushButton(name)
+            button.setFont(QFont("Arial", 14, QFont.Bold))
+            button.setStyleSheet("QPushButton { background-color: gray; border-radius: 20px; width: 100px; height: 60px; }")  # Set the buttons to gray, with round corners, shorter width, and taller height
+            buttons.append(button)
+
+        # Set the first button to red
+        buttons[0].setStyleSheet("QPushButton { background-color: red; border-radius: 20px; width: 100px; height: 60px; }")  # Set the first button to red, with round corners, shorter width, and taller height
+
+        # Connect the button behavior
+        for i in range(4):
+            buttons[i].clicked.connect(lambda _, next_button=buttons[i+1]: self.enable_next_button(next_button))
+
+        # Add the buttons to the button layout
+        for button in buttons:
+            buttonLayout.addWidget(button)
+
+        # Add the button layout to the picture and buttons layout
+        pictureAndButtonsLayout.addLayout(buttonLayout)
+
+        # Add the picture and buttons layout to the main layout
+        self.layout.addLayout(pictureAndButtonsLayout)
+
+        self.setLayout(self.layout)
+
+    # Enable the next button when the current button is clicked
+    def enable_next_button(self, next_button):
+        next_button.setStyleSheet("QPushButton { background-color: red; border-radius: 20px; width: 50px; height: 60px; }")  # Set the button to red, with round corners, shorter width, and taller height
+        next_button.setEnabled(True)
